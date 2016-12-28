dbe74ff3e316476f0e5225dfb654cb09aa527683
Yerlikaya <o.yerlikaya07@gmail.com>
2016-12-28 15:57:26 +0300

    json converter
    son

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace HelloWorld
 {
@@ -11,7 +12,14 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+
+            Lesson ls = new Lesson();
+            ls.Name = "c++";
+            ls.Code = "bu bir kod örneğidir.";
+            string json = JsonConvert.SerializeObject(ls);
+            Console.WriteLine(json);
             Console.Read();
+
         }
     }
 }
