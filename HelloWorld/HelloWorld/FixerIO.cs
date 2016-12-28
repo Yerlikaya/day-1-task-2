57d2e1e72f5e5b8413736ccf241ad3f683618482
Yerlikaya <o.yerlikaya07@gmail.com>
2016-12-28 23:35:21 +0300

    TASK5 TAMAMLANDI NEXT LEVEL?

---
+++ HelloWorld/HelloWorld/FixerIO.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class FixerIO
+    {
+        public string Base { get; set; }
+        public string date { get; set; }
+        public string url { get; set; }
+        public Dictionary<string, double> rates;
+
+        public FixerIO fixSample;
+        public void CurrenciyleriCekVeAyristir()
+        {
+            try
+            {
+                string jsontext = new WebClient().DownloadString(url);
+
+                fixSample = JsonConvert.DeserializeObject<FixerIO>(jsontext);
+            }
+
+            catch
+            {
+                Console.WriteLine("ilk girilen döviz verisi bulunamadı.");
+            }
+           
+        }
+
+        public void CurrencyHesapla(int miktar,String ilkCur,String ikinciCur)
+        {
+            double d2 = fixSample.rates[ikinciCur];
+            double sonuc = d2 * miktar;
+            Console.WriteLine(sonuc+" "+ikinciCur);
+
+
+
+        }
+    }
+}
--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -16,10 +16,32 @@ namespace HelloWorld
             task2();
             task3();
             task4();
-
+            task5();
             Console.Read();
         }
+        public static void task5()
+        {
+            Console.WriteLine("\nçevirmek istediğiniz döviz birimini girin.\n örn: 120-TRY-USD");
+            string birimler= Console.ReadLine();
+            try
+            {
+                string[] doviz = birimler.Split('-');
+                string m = doviz[0];
+                string d1 = doviz[1];
+                string d2 = doviz[2];
 
+                FixerIO fix = new FixerIO
+                {
+                    url = "http://api.fixer.io/latest?base=" + d1
+                };
+                fix.CurrenciyleriCekVeAyristir();
+                fix.CurrencyHesapla(int.Parse(m), d1, d2);
+            }
+            catch
+            {
+                Console.WriteLine("Belirtilen şekilde giriş yapılmadı!");
+            }
+        }
         public static void task4()
         {
             Currency currency = new Currency
