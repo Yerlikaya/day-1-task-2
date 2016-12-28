09c71cc2677636bdfac70ba038bac83114d9e4e3
Yerlikaya <o.yerlikaya07@gmail.com>
2016-12-28 16:40:18 +0300

    task4
    json veri string olarak çekilip yaratılan Currency sınıfına göre
    ayrıştırıldı.

---
+++ HelloWorld/HelloWorld/Currency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public class Currency
+    {
+        public string cc { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        
+
+
+    }
+}
--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.IO;
+using System.Net;
 
 namespace HelloWorld
 {
@@ -22,6 +24,18 @@ namespace HelloWorld
             string json = JsonConvert.SerializeObject(ls);
 
             Console.WriteLine(json);
+
+            string jsontext = new WebClient().DownloadString("https://raw.githubusercontent.com/mhs/world-currencies/master/currencies.json");
+
+            //Console.WriteLine(jsontex);
+            var cur = JsonConvert.DeserializeObject<List<Currency>>(jsontext);
+
+            foreach (var data in cur)
+            {
+                Console.WriteLine(data.cc+"\t"
+                    +data.symbol+ "\t"
+                    +data.name);
+            }
 
             Console.Read();
         }
